58e4b6c75c6fdb97d8204956dac01d2b0659a6bb
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-07-04 12:08:08 +0800

    add the close button to tle cart and shift the layout component when you click close

--- src/stores/cart.jsx
+++ src/stores/cart.jsx
@@ -1,6 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit';
 const initialState = {
-    items: []
+    items: [],
+    statusTab: false
 }
 const cartSlice = createSlice({
     name: 'cart',
@@ -14,8 +15,25 @@ const cartSlice = createSlice({
             } else {
                 state.items.push({productId, quantity});    
             }
+        },
+        changeQuantity(state, action){
+             const {productId, quantity} = action.payload;
+             const indexProductId = (state.items).findIndex(item => item.productId === productId);
+             if (quantity > 0) {
+                state.items[indexProductId].quantity = quantity;
+             } else {
+                //delete state.items[indexProductId];
+                state.items = (state.items).filter(item => item.productId !== productId);
+             }
+        },
+        toggleStatusTab(state){
+            if (state.statusTab === false) {
+                state.statusTab = true;
+            } else {
+                state.statusTab = false;
+            }
         }
     }
 })
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, changeQuantity, toggleStatusTab } = cartSlice.actions;
 export default cartSlice.reducer;
